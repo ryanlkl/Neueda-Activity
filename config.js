a8e9c67f6401c8a09d4800add71d08cb16250a39
Mahmood El-Mahalawy <mxhmoodem@hotmail.com>
2025-08-21 11:27:10 +0100

    Add db_pass and db_host to config
    Extended the config object to include database password and host from environment variables for improved database configuration flexibility.

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ export const config = {
   port: process.env.PORT,
   db_name: process.env.DB_NAME,
   db_user: process.env.DB_USER,
+  db_pass: process.env.DB_PASS,
+  db_host: process.env.DB_HOST
 }
